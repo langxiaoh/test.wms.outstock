62d1abf5241a0e01e39651f3df2d55cd76e19d0c
langxiaohua <langxiaoh@163.com>
2019-08-14 22:01:16 +0800

    初始化项目

---
+++ test.wms.outstock/test.wms.outstock.web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace test.wms.outstock.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
